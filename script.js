e654a307ba125a693ece2e472572672aa2832ed8
tamatamatamaki <52862676+tamatamatamaki@users.noreply.github.com>
2019-08-07 09:07:38 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+$(function() {
+  $('.top-title').hover(
+    function() {
+      $(this).animate({
+        'font-size':'75px'
+      }, 300);
+    },
+    function() {
+      $(this).animate({
+        'font-size':'50px'
+      }, 300);
+    }
+  )
+});
+
+
+// function() {
+  //   $(this).css('font-size','30px');
+  // },
+  // function() {
+    //   $(this).css('font-size'.'24px');
+    // }
+
+
+
+
+
+                // function() {
+                  //     $(this).css('color','red');
+                  //   },
+                  //   function() {
+                    //       $(this).css('color','white');
+                    //     }
