a45a408d10fc9bacd2a7fcbc227883d01fd6e4b2
tamatamatamaki <52862676+tamatamatamaki@users.noreply.github.com>
2019-08-09 16:15:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,28 @@ $(function() {
       }, 300);
     }
   )
+  $('.top-title-tab').hover(
+    function() {
+      $(this).animate({
+        'font-size':'45px'
+      }, 300);
+    },
+    function() {
+      $(this).animate({
+        'font-size':'30px'
+      }, 300);
+    }
+  )
+  $('.top-title-smart').hover(
+    function() {
+      $(this).animate({
+        'font-size':'30px'
+      }, 300);
+    },
+    function() {
+      $(this).animate({
+        'font-size':'20px'
+      }, 300);
+    }
+  )
 });
